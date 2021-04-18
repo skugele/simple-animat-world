4b0efb9fad44ecdd4dc87f6cb4dbda32d8ca08e8
Sean Kugele <seankugele@gmail.com>
2021-04-17 20:04:01 -0500

    Adds godot server launch,stop,status scripts

---
+++ scripts/launch_godot.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# launches godot servers using supervise (see daemontools).
+proc_dir=/home/skugele/Development/simple-animat-world/scripts/daemontools
+
+shopt -s nullglob
+
+cd $proc_dir
+for instance in */; do
+	svok $instance
+	if [ $? -eq 0 ]
+	then
+		# bounce running instance (if needed)
+		svc -tu $instance
+	else
+		nohup supervise $instance 1>${instance}/supervise.log 2>&1 &
+	fi
+done
---
+++ scripts/stop_godot.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# stops godot servers
+proc_dir=/home/skugele/Development/simple-animat-world/scripts/daemontools
+
+shopt -s nullglob
+
+cd $proc_dir
+
+for instance in */; do
+        svc -d $instance
+done
